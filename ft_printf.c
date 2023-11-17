897dc7ad01f9c6630a5be0bcee560a2cba406c1c
Teo Georgiev <tgeorgie@c2r4p5.hive.fi>
2023-11-17 09:28:12 +0200

    Refactoring of control functions. Removed counter function and simplified the control function. Removed issue with percentage sign

--- ft_printf.c
+++ ft_printf.c
@@ -6,97 +6,60 @@
 /*   By: tgeorgie <tgeorgie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 10:11:19 by tgeorgie          #+#    #+#             */
-/*   Updated: 2023/11/16 14:59:51 by tgeorgie         ###   ########.fr       */
+/*   Updated: 2023/11/17 08:48:38 by tgeorgie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./ft_printf.h"
-#include <stdio.h>
 
-static size_t	f_num_args(const char *str)
+static int	f_print_format(char format, va_list args)
 {
-	size_t		num;
-	size_t		i;
-	char const	*set;
-
-	num = 0;
-	i = 0;
-	set = "cspdiuxX%";
-	while (str[i])
+	if (ft_strchr("cspdiuxX%", format))
 	{
-		if (str[i] == '%' && ft_strchr(set, str[i + 1]))
-			num++;
-		i++;
+		if (format == 'd' || format == 'i' \
+			|| format == 'c')
+			return (f_print_int(format, va_arg(args, int)));
+		else if (format == 's')
+			return (f_print_str(va_arg(args, const char *)));
+		else if (format == 'p')
+			return (f_print_address(va_arg(args, unsigned long)));
+		else if (format == 'x')
+			return (f_print_hex(va_arg(args, unsigned int), ft_tolower));
+		else if (format == 'X')
+			return (f_print_hex(va_arg(args, unsigned int), ft_toupper));
+		else if (format == 'u')
+			return (f_print_unsigned_int(va_arg(args, unsigned int)));
+		else if (format == '%')
+			return (ft_putchar_fd('%', 1));
 	}
-	return (num);
+	else
+		return (ft_putchar_fd(format, 1));
+	return (-1);
 }
 
-static int	f_format(char format, va_list args)
+static int	f_print_control(va_list args, const char *str)
 {
-	if (format == 'd' || format == 'i' \
-		|| format == 'c')
-		return (f_print_int(format, va_arg(args, int)));
-	else if (format == 's')
-		return (f_print_str(va_arg(args, const char *)));
-	else if (format == 'p')
-		return (f_print_address(va_arg(args, unsigned long)));
-	else if (format == 'x')
-		return (f_print_hex(va_arg(args, unsigned int), ft_tolower));
-	else if (format == 'X')
-		return (f_print_hex(va_arg(args, unsigned int), ft_toupper));
-	else if (format == 'u')
-		return (f_print_unsigned_int(va_arg(args, unsigned int)));
-	else if (format == '%')
-		return (ft_putchar_fd('%', 1));
-	return (1);
-}
-
-static int	f_print_options(va_list args, const char *str, int len, int i)
-{
+	int	i;
+	int	len;
 	int	flag;
 
+	i = 0;
+	len = 0;
 	flag = 0;
 	while (str[i])
 	{
-		if (str[i] == '%' && str[i + 1] && ft_strchr("cspdiuxX%", str[i + 1]))
+		if (str[i] == '%' && str[i + 1])
 		{
-			flag = f_format(str[i + 1], args);
-			if (flag == -1)
-				return (-1);
-			len += flag;
+			flag = f_print_format(str[i + 1], args);
 			i += 2;
 		}
-		else if (str[i] == '%' && !str[i + 1])
-			return (len);
 		else if (str[i] != '%')
-		{
-			if (ft_putchar_fd(str[i], 1) == -1)
-				return (-1);
-			len++;
-			i++;
-		}
-	}
-	return (len);
-}
-
-static int	f_noformat(const char *str)
-{
-	int	len;
-	int	i;
-	int	flag;
-
-	len = 0;
-	i = -1;
-	flag = 0;
-	while (str[++i])
-	{
-		if (str[i] != '%')
-		{
-			flag = ft_putchar_fd(str[i], 1);
-			if (flag == -1)
-				return (-1);
-			len += flag;
-		}
+			flag = ft_putchar_fd(str[i++], 1);
+		else if (str[i] == '%' && !str[i + 1])
+			break ;
+		if (flag == -1)
+			return (-1);
+		len += flag;
 	}
 	return (len);
 }
@@ -104,14 +67,10 @@ static int	f_noformat(const char *str)
 int	ft_printf(const char *str, ...)
 {
 	va_list	args;
-	int		num_args;
 	int		len;
 
 	va_start(args, str);
-	num_args = f_num_args(str);
-	if (!num_args)
-		return (f_noformat(str));
-	len = f_print_options(args, str, 0, 0);
+	len = f_print_control(args, str);
 	va_end(args);
 	return (len);
 }
--- tests/main.c
+++ tests/main.c
@@ -6,7 +6,7 @@
 /*   By: tgeorgie <tgeorgie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 10:11:19 by tgeorgie          #+#    #+#             */
-/*   Updated: 2023/11/16 14:35:44 by tgeorgie         ###   ########.fr       */
+/*   Updated: 2023/11/17 08:39:45 by tgeorgie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,103 +19,94 @@ void	separator(void)
 	ft_printf("...........................\n");
 }
 
-void	test_d(char *str)
-{
-	ft_printf("%d\n", ft_printf("Number:\t\t%d\n", 10));
-	printf("%d\n", printf("Number:\t\t%d\n", 10));
-	separator();
-	ft_printf("%d\n", ft_printf("Number:\t\t%d\n", INT_MAX));
-	printf("%d\n", printf("Number:\t\t%d\n", INT_MAX));
-	separator();
-	ft_printf("%d\n", ft_printf("Number:\t\t%d\n", INT_MIN));
-	printf("%d\n", printf("Number:\t\t%d\n", INT_MIN));
-	separator();
-	ft_printf("%d\n", ft_printf("Number:\t\t%d\n", 0));
-	printf("%d\n", printf("Number:\t\t%d\n", 0));
-	separator();
-}
+// void	test_d(char *str)
+// {
+// 	ft_printf("%d\n", ft_printf("Number:\t\t%d\n", 10));
+// 	printf("%d\n", printf("Number:\t\t%d\n", 10));
+// 	separator();
+// 	ft_printf("%d\n", ft_printf("Number:\t\t%d\n", INT_MAX));
+// 	printf("%d\n", printf("Number:\t\t%d\n", INT_MAX));
+// 	separator();
+// 	ft_printf("%d\n", ft_printf("Number:\t\t%d\n", INT_MIN));
+// 	printf("%d\n", printf("Number:\t\t%d\n", INT_MIN));
+// 	separator();
+// 	ft_printf("%d\n", ft_printf("Number:\t\t%d\n", 0));
+// 	printf("%d\n", printf("Number:\t\t%d\n", 0));
+// 	separator();
+// }
 
-void	test_c(char *str)
-{
-	ft_printf("Character tests:\n");
-	ft_printf(" %c %c %c ", '0', 0, '1');
-	printf(" %c %c %c ", '0', 0, '1');
-	//ft_printf("%d\n", ft_printf(" %c %c %c ", '0', 0, '1'));
-	//printf("%d\n", printf(" %c %c %c ", '0', 0, '1'));
-	separator();
-	// ft_printf("%d\n", ft_printf("Hex lower:\t%x\n", 123456789));
-	// printf("%d\n", printf("Hex lower:\t%x\n", 123456789));
-	// separator();
-	// ft_printf("%d\n", ft_printf("Hex upper:\t%X\n", 123456789));
-	// printf("%d\n", printf("Hex upper:\t%X\n", 123456789));
-	// separator();
-	// ft_printf("%d\n", ft_printf("Unsigned int:\t%u\n", -10));
-	// printf("%d\n", printf("Unsigned int:\t%u\n", -10));
-	// separator();
-}
+// void	test_c(char *str)
+// {
+// 	ft_printf("Character tests:\n");
+// 	ft_printf(" %c %c %c ", '0', 0, '1');
+// 	printf(" %c %c %c ", '0', 0, '1');
+// }
 
-void	test_s(char *str)
-{
-	ft_printf("String tests:\n");
-	ft_printf("%d\n", ft_printf(" NULL %s NULL ", NULL));
-	separator();
-	ft_printf("%d\n", ft_printf(" %s", "can it handle \t and \n?"));
-	printf("%d\n", printf(" %s", "can it handle \t and \n?"));
-	return ;
-}
+// void	test_s(char *str)
+// {
+// 	ft_printf("String tests:\n");
+// 	ft_printf("%d\n", ft_printf(" NULL %s NULL ", NULL));
+// 	separator();
+// 	ft_printf("%d\n", ft_printf(" %s", "can it handle \t and \n?"));
+// 	printf("%d\n", printf(" %s", "can it handle \t and \n?"));
+// 	return ;
+// }
 
-void	test_p(char *str)
-{
-	ft_printf("Address tests:\n");
-	ft_printf("%d\n", ft_printf("Address:\t%p\n", str));
-	printf("%d\n", printf("Address:\t%p\n", str));
-	separator();
-	ft_printf("%d\n", ft_printf(" %p ", ""));
-	printf("%d\n", printf(" %p ", ""));
-	separator();
-	return ;
-}
+// void	test_p(char *str)
+// {
+// 	ft_printf("Address tests:\n");
+// 	ft_printf("%d\n", ft_printf("Address:\t%p\n", str));
+// 	printf("%d\n", printf("Address:\t%p\n", str));
+// 	separator();
+// 	ft_printf("%d\n", ft_printf(" %p ", ""));
+// 	printf("%d\n", printf(" %p ", ""));
+// 	separator();
+// 	return ;
+// }
 
-void	test_x(char *str)
-{
-	return ;
-}
+// void	test_x(char *str)
+// {
+// 	return ;
+// }
 
-void	test_u(char *str)
-{
-	return ;
-}
+// void	test_u(char *str)
+// {
+// 	return ;
+// }
 
-void	test_percent(char *str)
-{
-	return ;
-}
+// void	test_percent(char *str)
+// {
+// 	return ;
+// }
 
-void	test_mix(char *str)
-{
-	return ;
-}
+// void	test_mix(char *str)
+// {
+// 	return ;
+// }
 
-void	test_no_format(char *str)
-{
-	ft_printf("No format tests:\n");
-	ft_printf("%d\n", ft_printf("lorem%mipsum"));
-	printf("%d\n", printf("lorem%mipsum"));
-	separator();
-	ft_printf("%d\n", ft_printf("loremipsum%"));
-	printf("%d\n", printf("loremipsum%"));
-	separator();
-	return ;
-}
+// void	test_no_format(char *str)
+// {
+// 	ft_printf("No format tests:\n");
+// 	ft_printf("%d\n", ft_printf("lorem%mipsum"));
+// 	printf("%d\n", printf("lorem%mipsum"));
+// 	separator();
+// 	ft_printf("%d\n", ft_printf("loremipsum%"));
+// 	printf("%d\n", printf("loremipsum%"));
+// 	separator();
+// 	return ;
+// }
 
 
 int	main(void)
 {
 	char	*str;
+	int		test1; 
 
 	str = "Lorem ipsum dolor";
-	test_no_format(str);
-	//test_p(str);
-	//test_no_format("\x01\x02\a\v\b\f\r");
+	test1 = ft_printf("%c %s %p %d %i %u %x %X %% %jkjfhsd %\n%", 'z', "lorem ipsum", NULL, 120, 55, 99, 189, 189);
+	// test2 = printf("%c %s %p %d %i %u %x %X %% dkjfhsd \n", 'z', "lorem ipsum", NULL, 120, 55, 99, 189, 189);
+	// ft_printf("test1: %d\t\ttest2: %d\n", test1, test2);
+	// printf("test1: %d\t\ttest2: %d\n", test1, test2);
+	
 	return (0);
 }
